62cd3164f73ac7b389b00bfd3ccfc60e7dab52b4
Ben Robinson <ben@click-on.co.uk>
2023-03-14 22:52:29 +0000

    finished

--- index.js
+++ index.js
@@ -2,7 +2,11 @@
 // where your node app starts
 
 // init project
+
+var https = require('https');
+var fs = require('fs');
 var express = require('express');
+require('dotenv').config();
 var app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
@@ -18,15 +22,36 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
+app.get("/api/", (req, res) => {
+  let date = new Date(Date.now());  
+  res.json({
+    unix: date.getTime(),
+    utc: date.toUTCString()
+  });
+});
 
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get("/api/:dateString", (req, res) => {
+  const timeStamp = Number(req.params.dateString);  
+  let date = isNaN(timeStamp) ? new Date(req.params.dateString) : new Date(timeStamp); 
+  if (date instanceof Date && !isNaN(date.getTime())) {
+    res.json({
+      unix: date.getTime(),
+      utc: date.toUTCString()
+    });
+  } 
+  else{
+    res.json({ error : "Invalid Date" });
+  }
+  
 });
 
 
 
-// listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
-});
+const options = {
+  key: fs.readFileSync(process.env.SSL_KEY_PATH),
+  cert: fs.readFileSync(process.env.SSL_CERT_PATH)
+}
+
+const PORT = process.env.PORT || 443
+const listener = https.createServer(options, app).listen(PORT, console.log(`Node.js listening on port  ${PORT}`))
