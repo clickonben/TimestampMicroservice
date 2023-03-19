d9036690cab60b480eadffb9a6e408fbad36ec01
Ben Robinson <ben@click-on.co.uk>
2023-03-19 13:44:57 +0000

    changed to use http on port 3000

--- index.js
+++ index.js
@@ -1,23 +1,11 @@
-// index.js
-// where your node app starts
-
-// init project
-
-var https = require('https');
-var fs = require('fs');
 var express = require('express');
 require('dotenv').config();
 var app = express();
 
-// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
 var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
 
-// http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
-// http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
@@ -30,7 +18,6 @@ app.get("/api/", (req, res) => {
   });
 });
 
-// your first API endpoint... 
 app.get("/api/:dateString", (req, res) => {
   const timeStamp = Number(req.params.dateString);  
   let date = isNaN(timeStamp) ? new Date(req.params.dateString) : new Date(timeStamp); 
@@ -46,12 +33,7 @@ app.get("/api/:dateString", (req, res) => {
   
 });
 
+const listener = app.listen(process.env.PORT || 3000, () => {
+  console.log('Your app is listening on port ' + listener.address().port)
+})
 
-
-const options = {
-  key: fs.readFileSync(process.env.SSL_KEY_PATH),
-  cert: fs.readFileSync(process.env.SSL_CERT_PATH)
-}
-
-const PORT = process.env.PORT || 443
-const listener = https.createServer(options, app).listen(PORT, console.log(`Node.js listening on port  ${PORT}`))
